d1a3210245a2cdb203b88e251558eb06a4617800
AkiMadie16 <madie86@gmail.com>
2022-12-09 12:56:08 +1030

    two components and fontawesome added

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,7 +1,25 @@
 export default function AboutMe() {
   return (
-    <div>
-      <h1>hello world 1</h1>
+    <div className="container">
+      <h1>About me</h1>
+
+      <div>
+        skills
+        <div className="card-body">
+          <a
+            href="https://www.linkedin.com/in/madhavi-m-014b79b2/"
+            className="btn btn-outline-primary"
+          >
+            Visit Website
+          </a>
+          <a
+            href="https://github.com/AkiMadi16/happy-feet-app"
+            className="btn btn-outline-primary"
+          >
+            GitHub
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
